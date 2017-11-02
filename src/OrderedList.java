f198c63bf8b3559bfd01c9febf7d251120d61739
tewan2657 <tewan2657@HRH-IC0027326.wrdsb.ca>
2017-11-02 09:58:19 -0400

    New assignment

---
+++ src/OrderedList.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tewan2657
+ */
+public class OrderedList {
+
+    private IntNode head;
+    private int numItems;
+
+    public OrderedList() {
+    }
+
+    public void add(int num) {
+        
+        
+    }
+
+    public void remove(int num) {
+        
+        
+    }
+
+    public int size() {
+        return numItems;
+    }
+
+    public boolean isEmpty() {
+        return numItems == 0;
+    }
+
+    public int get(int pos) {
+        return 0;
+    }
+
+/**
+ * @param args the command line arguments
+ */
+public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
